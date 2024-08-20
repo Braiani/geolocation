c8073bdce58c8ba5badbdb9574a1101258cde349
Felipe Braiani <felipe.santos@ifms.edu.br>
2024-08-20 18:13:48 -0400

    First commit

---
+++ geolocation.py
@@ -0,0 +1,44 @@
+import requests
+
+def fazer_requisicao(url, data=[]):
+    try:
+        # adicionar o body com o json da requisição
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # Verifica se houve algum erro na requisição
+        return response.json()  # Retorna a resposta da API em formato JSON
+    except requests.exceptions.RequestException as e:
+        print(f"Ocorreu um erro na requisição: {e}")
+        return None
+
+def preparar_range_ips(lista):
+    data = []
+    for ip in lista:
+        data.append({"query": ip, "fields": "city,country,countryCode,query", "lang": "pt-BR" })
+    return data
+
+def executar_geolocation(url, lista):
+    data = preparar_range_ips(lista)
+    return fazer_requisicao(url, data)
+
+if __name__ == "__main__":
+    url = "http://ip-api.com/batch"
+    arquivo = "lista_ips.txt"
+    ips = []
+    try:
+        with open(arquivo, "r") as file:
+            # Exemplo de ips dentro do arquivo: 24.152.88.0/24, 24.152.17.0/24, 24.152.10.0/24
+            ips = file.readlines()
+            ips = [ip.strip() for ip in ips]
+    except FileNotFoundError:
+        print(f"Arquivo {arquivo} não encontrado")
+
+    if ips:
+        range_ips = []
+        for ip in ips:
+            if "/32" in ip:
+                range_ips.append(ip.replace("/32", ""))
+
+        dados = executar_geolocation(url, range_ips)
+        if dados:
+            for dado in dados:
+                print(f"IP: {dado['query']}, Cidade: {dado['city']}, País: {dado['country']} ({dado['countryCode']})")
